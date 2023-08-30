b4b9065791f8848389d6d2ccbc826c8517981597
Paul Ockenfuss <paul.ockenfuss@campus.lmu.de>
2023-08-30 13:52:29 +0200

    Remove duplicates in Flowchart -Duplicate arrows are generated if two dependencies have similarities in their history.

--- inlog/flowchart.py
+++ inlog/flowchart.py
@@ -4,17 +4,18 @@ import json
 from pathlib import Path
 import hashlib
 
+MERMAID_CODE=[]
 
 def make_nodes_noloop(jlog, filename, id):
     filename=Path(filename).name
     self_id=id
-    print(f"    id{self_id}[{filename}]")
+    MERMAID_CODE.append(f"    id{self_id}[{filename}]")
     if "dependencies" in jlog:
         for name, dep in jlog["dependencies"].items():
             dep_id=id+1
             id=make_nodes(dep, name, dep_id)
             progamname=Path(jlog["program"]).name
-            print(f"    id{dep_id} --> |{progamname}| id{self_id}")
+            MERMAID_CODE.append(f"    id{dep_id} --> |{progamname}| id{self_id}")
     return id
 
 def _hash_name(name):
@@ -23,21 +24,23 @@ def _hash_name(name):
 def make_nodes(jlog, filename):
     self_hash=_hash_name(filename)
     filename=Path(filename).name
-    print(f"    id_{self_hash}[{filename}]")
+    MERMAID_CODE.append(f"    id_{self_hash}[{filename}]")
     if "dependencies" in jlog:
         for name, dep in jlog["dependencies"].items():
             dep_hash=_hash_name(name)
             make_nodes(dep, name)
             progamname=Path(jlog["program"]).name
-            print(f"    id_{dep_hash} --> |{progamname}| id_{self_hash}")
+            MERMAID_CODE.append(f"    id_{dep_hash} --> |{progamname}| id_{self_hash}")
 
 jlog={}
 with open(sys.argv[1], 'r') as f:
     jlog.update(json.load(f))
 
-print("flowchart TD")
+MERMAID_CODE.append("flowchart TD")
 # make_nodes_noloop(jlog, sys.argv[1], 1)
 make_nodes(jlog, sys.argv[1])
+MERMAID_CODE=list(dict.fromkeys(MERMAID_CODE)) #remove duplicates
+print("\n".join(MERMAID_CODE))
 
 
         
