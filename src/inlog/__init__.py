226b5b8040a68797196e68cf4c200b99155c4a17
Paul Ockenfuss <paul.ockenfuss@campus.lmu.de>
2023-10-09 15:50:33 +0200

    Allow file handles to be passed

--- src/inlog/__init__.py
+++ src/inlog/__init__.py
@@ -1,5 +1,5 @@
 from .Logger import Logger
-from .loaders import load_yaml, load_ini
+from .loaders import load_yaml, load_ini, load_json
 __version__ = "2.0.0"
 
 
--- src/inlog/loaders.py
+++ src/inlog/loaders.py
@@ -1,5 +1,6 @@
 from .Logger import Logger
 import configparser
+from pathlib import Path
 import json
 try:
     import yaml
@@ -17,27 +18,86 @@ def _configparser_to_dict(configparser):
         return options
 
 def load_ini(ini_file, version, def_opts={}):
+    """Create a logger object from an ini file
+
+    Parameters
+    ----------
+    ini_file : str, path-like or file-like
+        Path to the ini file or file-like object
+    version : str
+        Version of your program
+    def_opts : dict, optional
+        Default parameters, used as a fallback if not present in the config file. by default {}
+
+    Returns
+    -------
+    Logger
+        The logger object
+    """
     config = configparser.ConfigParser()
     config._interpolation = configparser.ExtendedInterpolation()
-    config.read(ini_file)
+    try: #ini_file is a string of path
+        p=Path(ini_file)
+        config.read(p)
+    except TypeError:
+        config.read_file(ini_file) #ini_file is a file-like object
     config_dict= _configparser_to_dict(config)
     logger=Logger(config_dict, version, def_opts=def_opts)
     logger.filename=ini_file
     return logger
 
 def load_yaml(yaml_file, version, def_opts={}):
+    """Create a logger object from a yaml file
+
+    Parameters
+    ----------
+    yaml_file : str, path-like or file-like
+        Path to the yaml file or file-like object
+    version : str
+        Version of your program
+    def_opts : dict, optional
+        Default parameters, used as a fallback if not present in the config file. by default {}
+
+    Returns
+    -------
+    Logger
+        The logger object
+    """
     if not _has_yaml:
         raise ImportError("You need to install pyyaml to use yaml files (contained in inlog[extras])")
-
-    with open(yaml_file) as f:
-        config_dict = yaml.safe_load(f)
+    try:
+        p=Path(yaml_file)
+        with open(p) as f:
+            config_dict = yaml.safe_load(f)
+    except TypeError:
+        config_dict = yaml.safe_load(yaml_file)
     logger=Logger(config_dict, version, def_opts=def_opts)
     logger.filename=yaml_file
     return logger
 
 def load_json(json_file, version, def_opts={}):
-    with open(json_file) as f:
-        config_dict = json.load(f)
+    """Create a logger object from a json file
+
+    Parameters
+    ----------
+    json_file : str, path-like or file-like
+        Path to the json file or file-like object
+    version : str
+        Version of your program
+    def_opts : dict, optional
+        Default parameters, used as a fallback if not present in the config file. by default {}
+
+    Returns
+    -------
+    Logger
+        The logger object
+    """
+    try:
+        p=Path(json_file)
+        with open(p) as f:
+            config_dict = json.load(f)
+    except TypeError:
+        config_dict = json.load(json_file)
     logger=Logger(config_dict, version, def_opts=def_opts)
     logger.filename=json_file
     return logger
